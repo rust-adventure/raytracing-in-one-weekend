f4e9a3627c62514eb4fbe1171a31206591498018
Christopher Biscardi <chris@christopherbiscardi.com>
2023-08-26 08:29:54 -0700

    progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use indicatif::ProgressIterator;
 use itertools::Itertools;
 use std::{fs, io};
 
@@ -8,6 +9,9 @@ const MAX_VALUE: u8 = 255;
 fn main() -> io::Result<()> {
     let pixels = (0..IMAGE_HEIGHT)
         .cartesian_product(0..IMAGE_WIDTH)
+        .progress_count(
+            IMAGE_HEIGHT as u64 * IMAGE_WIDTH as u64,
+        )
         .map(|(y, x)| {
             let r = x as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = y as f64 / (IMAGE_HEIGHT - 1) as f64;
